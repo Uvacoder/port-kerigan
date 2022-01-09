c808099175bcdb969f8a9b72fce158f56851ebe1
Kaushal K R <kaushalkr27@gmail.com>
2022-01-08 16:31:52 -0800

    first commit

---
+++ content/projects/dual-axis-solar-detector.md
@@ -0,0 +1,10 @@
+---
+id: 4
+title: dual axis solar detector
+description: a device that can detect the maximum intensity of light at any point of the day & orient the solar panel towards that direction. captures 44% more energy compared to the traditional mounted solar panels.
+tech: arduino LDRs servo
+logo: /images/lookatx-dev.png
+website: 
+github: 
+twitter: 
+---
---
+++ karngyan.config.js
@@ -0,0 +1,135 @@
+// Upload files in static/ So any file static/file.pdf -> /file.pdf is accessible at root
+
+export default {
+  name: 'kaushal k r',
+  domain: 'kaushalkr.com', // add without https:// , used in meta tags and share urls
+  image: '/images/bigheadkarngyan.png',
+  email: 'kaushalkr27@gmail.com',
+  googleAnalyticsV4: {
+    enabled: false,
+    id: ''
+  },
+  plausibleAnalytics: {
+    enabled: true,
+    domain: 'template.karngyan.com',
+    trackLocalhost: false,
+    // leave it empty if plausible is not self hosted
+    apiHost: 'https://analytics.lookatx.dev' // default: https://plausible.io
+  },
+  // enable if you want comments and likes on posts
+  // see how it looks on karngyan.com
+  firebase: {
+    enabled: false
+  },
+  social: {
+    github: 'kaushalkr27',
+    linkedin: 'kaushalkr',
+    facebook: 'kaushal.kr.73',
+    twitter: '',
+    instagram: 'kaushal_k_r',
+    codestats: 'kaushalkr27' // https://codestats.net make a profile if you dont already have one.
+  },
+  buyMeACoffee: {
+    enabled: false,
+    url: 'https://www.buymeacoffee.com/karngyan'
+  },
+  projects: {
+    enabled: true,
+  },
+  blog: {
+    enabled: false,
+  },
+  resume: {
+    enabled: false,
+    // pdfUrl: '/RESUME_GYAN_KARN_DARK.pdf' // add files in static folder
+  },
+  uses: {
+    enabled: true,
+    meta: [
+      {title: 'OS', value: 'macOS Monterey'},
+      {title: 'Memory', value: '16GB unified memory 256GB SSD storage'},
+      {title: 'Keyboard', value: 'Keychron K2'},
+      {title: 'Mouse', value: 'Razer Basilisk X Hyperspeed Wireless'},
+      {title: 'Monitor', value: 'HP Full HD (1920 x 1080) 24 inch IPS Display'},
+      {title: 'Laptop', value: 'MacBook Pro (13-inch, M1, 2020)'}
+    ]
+  },
+  workedAt: {
+    // add logos in static and at max add 3/4
+    enabled: false,
+    meta: [
+      { name: 'NeoScale.io', src: '/images/sendpost.png', url: 'https://neoscale.io/' },
+      { name: 'Streamlyn Media', src: '/images/streamlyn.png', url: 'https://streamlyn.com/' },
+      { name: 'Cuberootz', src: '/images/orinko.png', url: 'https://www.linkedin.com/company/theorinko/' },
+      { name: 'InMobi', src: '/images/inmobi.svg', url: 'https://inmobi.com' },
+    ]
+  },
+  recommendations: {
+    enabled: false,
+    meta: [
+      { name: 'Varun Jain', designation: 'Founder, SendX & SendPost', image: '/images/varun.jpeg', linkedin: 'https://www.linkedin.com/in/varun-jain-582b0215/', content: 'Gyan interned with SendX and SendPost for around 6 months. He is a very strong full-stack engineer who can create pixel perfect frontend while being able to work on backend pieces sending millions of emails per day. He is a quick learner, a great team player and, has a very strong work ethic. You can rely on him that things will be done with utmost quality. He also has pretty good product instincts and can think from the user\'s perspective.' },
+      { name: 'Gaurav Sen', designation: 'Founder, InterviewReady', image: '/images/gaurav.jpeg', linkedin: 'https://www.linkedin.com/in/gkcs/', content: 'Karn is an extraordinarily talented, diligent and foresighted individual. He is gifted at building products from paper to code, and has a knack of finding efficient solutions to complex problems. In my experience, I have rarely seen such a mixture of talent and passion put together. At InterviewReady, Karn exceeded our expectations. If you are considering working with him: congratulations, you are going to have a great time!' },
+    ]
+  },
+  loadingIndicator: {
+    name: 'pulse'
+    // https://tobiasahlin.com/spinkit/
+    // circle
+    // cube-grid
+    // fading-circle
+    // folding-cube
+    // chasing-dots
+    // nuxt
+    // pulse
+    // rectangle-bounce
+    // rotating-plane
+    // three-bounce
+    // wandering-cubes
+  },
+  strings: {
+    en_US: {
+      download: 'download',
+      nav: {
+        home: 'home',
+        blog: 'blog',
+        projects: 'projects',
+        uses: 'uses',
+        resume: 'résumé',
+        buyMeACoffee: 'buy me a beer',
+        signIn: 'sign in',
+        signOut: 'sign out'
+      },
+      hero: {
+        iBlogTech: 'i blog tech',
+        haveALook: 'have a look',
+        friendlyNeighborhood: 'your friendly neighborhood',
+        description: 'i am a software engineer with 3+ years of experience in delivering dynamic web services and IoT solutions tailored to enterprise clients\' precise needs.',
+        words: ['developer', 'engineer', 'programmer', 'freelancer'],
+      },
+      githubCalendar: {
+        header: 'contributions',
+        subtext: 'github calendar heatmap'
+      },
+      blog: {
+        header: 'blog',
+        subtext: 'i try to write once in a while. let me know your thoughts in comments or mail@karngyan.com'
+      },
+      recentBlog: {
+        header: 'recent blogs',
+        subtext: 'it takes a lot of time to write man'
+      },
+      uses: {
+        header: 'uses',
+        subtext: 'a quick summary of what I use on a daily basis to code and some codestats.net flex'
+      },
+      projects: {
+        header: 'projects',
+        subtext: 'this page lists some of my personal and work projects. every project has some story, click on the title to read'
+      },
+      recommendations: {
+        header: 'recommendations',
+        subtext: 'what do my mentors say'
+      }
+    }
+  }
+}
